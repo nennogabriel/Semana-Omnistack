8586dbfe4c0561f124f891cacc17f1e4021f99f0
nennogabriel <nennogabriel@gmail.com>
2020-01-15 22:54:55 -0300

    web app week 10

---
+++ 10-DevRadar/frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+import api from "./services/api";
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/Devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>{devs && devs.map(dev => <DevItem dev={dev} key={dev._id} />)}</ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
